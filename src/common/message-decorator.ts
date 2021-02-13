a7213d15f39eac96a753d8536d75f976b534bc25
Rob11001 <r.bruno49@studenti.unisa.it>
2021-02-13 12:18:58 +0100

    Added date string format

--- src/common/message-decorator.ts
+++ src/common/message-decorator.ts
@@ -1,25 +1,30 @@
 import Discord from 'discord.js';
 import {Event} from '../cache/session';
 
+const pads = (s: number): string => {
+  return s < 10 ? `0${s}` : `${s}`;
+};
+
 export default class MessageDecorator {
   public static async newEventMessage(client: Discord.Client, event: Event, isAuthor: boolean): Promise<Discord.MessageEmbed> {
     const guild = await client.guilds.fetch(event.guild);
     const member = await guild.members.fetch(event.author);
     const minutes = event.privateReminder % 60;
-    const zero = minutes < 10 ? '0' : '';
+    const date = event.date as Date;
+    const dateString = [pads(date.getDate()), pads(date.getMonth()), pads(date.getFullYear())].join('-');
 
     const embed = new Discord.MessageEmbed();
     embed.setTitle(event.name)
         .setDescription(event.description)
         .setAuthor(member.displayName, member.user.displayAvatarURL())
-        .addField('Date', event.date.toLocaleString())
+        .addField('Date', `${dateString} ${date.getHours()}:${pads(date.getMinutes())}`)
         .setColor('#FFD700')
         .setTimestamp();
 
     if (isAuthor) {
       embed.addFields(
           {name: 'Server reminder', value: `Every ${event.globalReminder} day(s)`, inline: true},
-          {name: 'Private reminder', value: `${Math.floor(event.privateReminder / 60)}:${zero}${minutes} hour(s) before the event`, inline: true},
+          {name: 'Private reminder', value: `${Math.floor(event.privateReminder / 60)}:${pads(minutes)} hour(s) before the event`, inline: true},
       );
     }
 
